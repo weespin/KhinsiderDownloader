7facd2257f0f336ec6d75aa762d584a966c6a4cd
William <wnmerukini@gmail.com>
2020-08-08 04:02:19 +0300

    for the last time :grimm:

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.urllist_box = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
@@ -144,6 +145,7 @@
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.urllist_box);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.Text = "KhinsiderDownloader";
 			this.Load += new System.EventHandler(this.Form1_Load);
