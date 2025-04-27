a268d0c69b94be64f52d26132646bd5ffa762a10
Viliam <wnmerukini@gmail.com>
2025-04-27 21:41:37 +0300

    Update Song.h

--- src/parser/Song.h
+++ src/parser/Song.h
@@ -10,14 +10,12 @@
 class Song : public QObject {
     Q_OBJECT
 
-    // Group related properties with a single NOTIFY signal
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY dataChanged)
     Q_PROPERTY(int lengthInSeconds READ lengthInSeconds WRITE setLengthInSeconds NOTIFY dataChanged)
     Q_PROPERTY(QMap<QString, unsigned long> size READ size WRITE setSize NOTIFY dataChanged)
     Q_PROPERTY(QString songLink READ songLink WRITE setSongLink NOTIFY dataChanged)
     Q_PROPERTY(QMap<QString, QString> downloadLinks READ downloadLinks WRITE setDownloadLinks NOTIFY dataChanged)
 
-    // State properties that can all use a single state change signal
     Q_PROPERTY(bool downloaded READ downloaded WRITE setDownloaded NOTIFY stateChanged)
     Q_PROPERTY(bool errored READ errored WRITE setErrored NOTIFY stateChanged)
     Q_PROPERTY(qint64 downloadedSize READ downloadedSize WRITE setDownloadedSize NOTIFY downloadProgressChanged)
@@ -29,24 +27,24 @@ public:
     explicit Song(QObject *parent = nullptr) : QObject(parent) {
     }
 
-    // Data accessors
+    // Data
     QString name() const { return QString::fromStdString(m_name); }
     int lengthInSeconds() const { return m_lengthInSeconds; }
     QMap<QString, unsigned long> size() const { return m_size; }
     QString songLink() const { return QString::fromStdString(m_songLink); }
     QMap<QString, QString> downloadLinks() const { return m_downloadLinks; }
 
-    // State accessors
+    // States
     bool downloaded() const { return m_downloaded; }
     bool errored() const { return m_erored; }
     qint64 downloadedSize() const { return m_downloadedSize; }
     qint64 downloadSpeed() const { return m_downloadSpeed; }
-    // Additional accessors
+
     bool getAllDownloadLinksParsed() const { return m_downloadLinksParsed; }
+    //Unused code
     QString activeDownloadLink() const { return m_activeDownloadLink; }
     QString activeDownloadPath() const { return m_activeDownloadPath; }
 
-    // Modified setters that reduce code duplication
     void cancel() {
         emit wantToCancel();
     }
@@ -121,11 +119,12 @@ public:
         emit downloadProgressChanged();
     }
 
-    // Additional setters
+
     void setAllDownloadLinksParsed(bool value) {
         m_downloadLinksParsed = value;
     }
 
+    // QT Creator: Unused code
     void setActiveDownloadLink(const QString &activeDownloadLink) {
         m_activeDownloadLink = activeDownloadLink;
     }
@@ -135,27 +134,23 @@ public:
     }
 
 signals:
-    // Reduced to just 3 signals instead of many individual ones
     void dataChanged(); // For metadata changes
     void stateChanged(); // For state changes
     void downloadProgressChanged(); // For download progress updates
     void wantToCancel();
 
 private:
-    // Metadata
     std::string m_name;
     int m_lengthInSeconds = 0;
     QMap<QString, unsigned long> m_size;
     std::string m_songLink;
     QMap<QString, QString> m_downloadLinks;
 
-    // State
     bool m_downloaded = false;
     bool m_erored = false;
     qint64 m_downloadedSize = 0;
     qint64 m_downloadSpeed = 0;
 
-    // Additional data
     bool m_downloadLinksParsed = false;
     QString m_activeDownloadLink;
     QString m_activeDownloadPath;
